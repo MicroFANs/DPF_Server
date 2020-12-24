82000d5b4743c77da76675700057ec35f2d65b07
范泽轩 <35322505+MicroFANs@users.noreply.github.com>
2020-12-24 16:46:08 +0800

    完善了连接服务器
    连接上可以用通知来显示

--- app.py
+++ app.py
@@ -1,17 +1,3 @@
-# from flask import Flask
-#
-# app = Flask(__name__)
-#
-#
-# @app.route('/')
-# def hello_world():
-#     return 'Hello World!'
-#
-#
-# if __name__ == '__main__':
-#     app.run()
-
-
 
 from flask import Flask
 from flask import request
@@ -26,7 +12,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 
 @app.route('/')
 def test():
-    return '服务器正常运行'
+    return '服务器正常运行!!!'
 
 
 
